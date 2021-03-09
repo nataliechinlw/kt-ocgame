b362b32e0c387516624cf42a86401790e52efac4
Natalie Chin <natalie.chin@thoughtworks.com>
2021-03-09 14:28:17 +0800

    Add mockito library
    Signed-off-by: Natalie Chin <natalie.chin@thoughtworks.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,8 @@ dependencies {
     testImplementation(kotlin("test-junit5"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    testImplementation("org.mockito:mockito-core:2.+")
+    testImplementation("org.mockito:mockito-inline:2.13.0")
 }
 
 tasks.test {
