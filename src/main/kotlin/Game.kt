f8fe8adaa46b9118d75c5053d714d15491b81ee1
Natalie Chin <natalie.chin@thoughtworks.com>
2021-03-09 16:32:47 +0800

    Should evaluate with no winner
    Signed-off-by: Natalie Chin <natalie.chin@thoughtworks.com>

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -2,4 +2,11 @@ class Game {
     fun start() {
         println("Welcome to the game!")
     }
+
+    fun evaluateWinner(playerInput: PlayerInput, aiInput: AiInput) {
+        val totalNumberOfOpenHands = playerInput.numberOfOpenHands + aiInput.numberOfOpenHands
+        val prediction = playerInput.prediction
+        if (totalNumberOfOpenHands != prediction)
+            println("No winner")
+    }
 }
--- src/test/kotlin/GameTest.kt
+++ src/test/kotlin/GameTest.kt
@@ -4,11 +4,16 @@ import java.io.PrintStream
 import org.junit.jupiter.api.BeforeEach
 import java.io.ByteArrayOutputStream
 import org.junit.jupiter.api.AfterEach
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
 
 internal class GameTest {
     private val testGame = Game()
     private val standardOut = System.out
     private val outputStreamCaptor = ByteArrayOutputStream()
+
+    private val playerInput = mock(PlayerInput::class.java)
+    private val aiInput = mock(AiInput::class.java)
 
     @BeforeEach
     fun setUp() {
@@ -24,5 +29,14 @@ internal class GameTest {
     internal fun `should print welcome message`() {
         testGame.start()
         assertEquals("Welcome to the game!", outputStreamCaptor.toString().trim())
+    }
+
+    @Test
+    internal fun `should evaluate no winner`() {
+        `when`(playerInput.numberOfOpenHands).thenReturn(1)
+        `when`(playerInput.prediction).thenReturn(1)
+        `when`(aiInput.numberOfOpenHands).thenReturn(1)
+        testGame.evaluateWinner(playerInput, aiInput)
+        assertEquals("No winner", outputStreamCaptor.toString().trim())
     }
 }
