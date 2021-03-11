d700de10d677cbf785cf9f676d2ee47c97b5a35c
Natalie Chin <natalie.chin@thoughtworks.com>
2021-03-11 16:34:41 +0800

    Add a tab for readability
    Signed-off-by: Natalie Chin <natalie.chin@thoughtworks.com>

--- src/main/kotlin/Terminal.kt
+++ src/main/kotlin/Terminal.kt
@@ -1,4 +1,7 @@
 object Terminal {
     fun printMessage(message: String) = println(message)
-    fun getInput() = readLine()
+    fun getInput(): String? {
+        print("\t")
+        return readLine()
+    }
 }
