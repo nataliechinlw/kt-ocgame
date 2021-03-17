fe7037d545d483da1f829b13836a122efa040196
Natalie Chin <natalie.chin@thoughtworks.com>
2021-03-17 08:58:52 +0800

    Create askForInputMessage in Player enum
    Signed-off-by: Natalie Chin <natalie.chin@thoughtworks.com>

--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,10 +1,15 @@
+const val ASK_FOR_INPUT = ", what is your input?"
+
 enum class Player {
     HUMAN {
         override fun getWinnerMessage() = "You WIN!!"
+        override fun getAskForInputMessage() = "You are the predictor$ASK_FOR_INPUT"
     },
     AI {
         override fun getWinnerMessage() = "AI WINS!!"
+        override fun getAskForInputMessage() = "AI is the predictor$ASK_FOR_INPUT"
     };
 
     abstract fun getWinnerMessage(): String
+    abstract fun getAskForInputMessage(): String
 }
--- src/test/kotlin/PlayerTest.kt
+++ src/test/kotlin/PlayerTest.kt
@@ -8,4 +8,10 @@ internal class PlayerTest {
     internal fun `should print correct winner message given winning Player`(playerEnum: String, winnerMessage: String) {
         assertEquals(winnerMessage, Player.valueOf(playerEnum).getWinnerMessage())
     }
+
+    @ParameterizedTest
+    @CsvSource("HUMAN; You are the predictor, what is your input?", "AI; AI is the predictor, what is your input?", delimiter = ';')
+    internal fun `should print correct ask for input message given current predicting Player`(playerEnum: String, askForInputMessage: String) {
+        assertEquals(askForInputMessage, Player.valueOf(playerEnum).getAskForInputMessage())
+    }
 }
