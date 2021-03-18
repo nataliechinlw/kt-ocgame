b41decbb0bfa60bd7ae2535d3b137cbd6ac580cb
Natalie Chin <natalie.chin@thoughtworks.com>
2021-03-18 17:28:19 +0800

    Remove redundant qualifier
    Signed-off-by: Natalie Chin <natalie.chin@thoughtworks.com>

--- src/test/kotlin/InputValidatorTest.kt
+++ src/test/kotlin/InputValidatorTest.kt
@@ -10,15 +10,15 @@ import kotlin.test.assertNull
 internal class InputValidatorTest {
     @Test
     internal fun `should validate yesNo responses`() {
-        kotlin.test.assertNull(yesNo("Y"))
-        kotlin.test.assertNull(yesNo("N"))
+        assertNull(yesNo("Y"))
+        assertNull(yesNo("N"))
         kotlin.test.assertEquals("input should be either Y or N", yesNo("X"))
     }
 
     @Test
     internal fun `should validate inputWithPrediction responses`() {
-        kotlin.test.assertNull(inputWithPrediction("OO0"))
-        kotlin.test.assertNull(inputWithPrediction("CC4"))
+        assertNull(inputWithPrediction("OO0"))
+        assertNull(inputWithPrediction("CC4"))
         kotlin.test.assertEquals(
             "correct input should be of the form CC3, where the first two letters indicate [O]pen or [C]losed state for each hand, followed by the prediction (0-4)",
             inputWithPrediction("chicken")
@@ -27,8 +27,8 @@ internal class InputValidatorTest {
 
     @Test
     internal fun `should validate inputWithoutPrediction responses`() {
-        kotlin.test.assertNull(inputWithoutPrediction("OO"))
-        kotlin.test.assertNull(inputWithoutPrediction("CC"))
+        assertNull(inputWithoutPrediction("OO"))
+        assertNull(inputWithoutPrediction("CC"))
         kotlin.test.assertEquals(
             "correct input should be of the form CC, where the first two letters indicate [O]pen or [C]losed state for each hand",
             inputWithoutPrediction("CC2")
