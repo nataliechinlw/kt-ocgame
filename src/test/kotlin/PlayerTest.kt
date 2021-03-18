96dc68b9c76182daa1d0b7b2e96dae409c5340ef
Natalie Chin <natalie.chin@thoughtworks.com>
2021-03-18 17:49:49 +0800

    Use implicit conversion when passing enums into tests
    Signed-off-by: Natalie Chin <natalie.chin@thoughtworks.com>

--- src/test/kotlin/PlayerTest.kt
+++ src/test/kotlin/PlayerTest.kt
@@ -16,14 +16,14 @@ internal class PlayerTest {
 
     @ParameterizedTest
     @CsvSource("HUMAN, You WIN!!", "AI, AI WINS!!")
-    internal fun `should print correct winner message given winning Player`(playerEnum: String, winnerMessage: String) {
-        assertEquals(winnerMessage, Player.valueOf(playerEnum).getWinnerMessage())
+    internal fun `should print correct winner message given winning Player`(player: Player, winnerMessage: String) {
+        assertEquals(winnerMessage, player.getWinnerMessage())
     }
 
     @ParameterizedTest
     @CsvSource("HUMAN; You are the predictor, what is your input?", "AI; AI is the predictor, what is your input?", delimiter = ';')
-    internal fun `should print correct ask for input message given current predicting Player`(playerEnum: String, askForInputMessage: String) {
-        assertEquals(askForInputMessage, Player.valueOf(playerEnum).getAskForInputMessage())
+    internal fun `should print correct ask for input message given current predicting Player`(player: Player, askForInputMessage: String) {
+        assertEquals(askForInputMessage, player.getAskForInputMessage())
     }
 
     @Test
