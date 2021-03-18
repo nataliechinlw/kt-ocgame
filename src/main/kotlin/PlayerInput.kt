cefc767e3b8954c92d225a8784825da664954440
Natalie Chin <natalie.chin@thoughtworks.com>
2021-03-18 14:53:47 +0800

    Make PlayerInput a data class
    Signed-off-by: Natalie Chin <natalie.chin@thoughtworks.com>

--- src/main/kotlin/PlayerInput.kt
+++ src/main/kotlin/PlayerInput.kt
@@ -1,4 +1,4 @@
-class PlayerInput(override val input: String, isPredictor: Boolean) : Input() {
+data class PlayerInput(override val input: String, val isPredictor: Boolean) : Input() {
     override val numberOfOpenHands = Regex("O").findAll(input).count()
     override val prediction = if (isPredictor) {
         input[2].toString().toInt()
