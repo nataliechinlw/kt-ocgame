8ede099e440ae322cac3cf16fa645da56b2f06ea
Natalie Chin <natalie.chin@thoughtworks.com>
2021-03-18 15:03:41 +0800

    Move askForInput into HUMAN generateInput
    Signed-off-by: Natalie Chin <natalie.chin@thoughtworks.com>

--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,9 +1,19 @@
+import InputValidator.inputWithPrediction
+import InputValidator.inputWithoutPrediction
+
 const val ASK_FOR_INPUT = ", what is your input?"
 
 enum class Player {
     HUMAN {
         override fun getWinnerMessage() = "You WIN!!"
         override fun getAskForInputMessage() = "You are the predictor$ASK_FOR_INPUT"
+        override fun generateInput(currentPredictor: Player): PlayerInput {
+            val message = currentPredictor.getAskForInputMessage()
+            Terminal.printMessage(message)
+            val inputValidator =
+                if (currentPredictor == HUMAN) ::inputWithPrediction else ::inputWithoutPrediction
+            return PlayerInput.create(Terminal.getInput(inputValidator), currentPredictor == HUMAN)
+        }
     },
     AI {
         override fun getWinnerMessage() = "AI WINS!!"
@@ -12,4 +22,8 @@ enum class Player {
 
     abstract fun getWinnerMessage(): String
     abstract fun getAskForInputMessage(): String
+    open fun generateInput(currentPredictor: Player): Input {
+        return AiInput()
+    }
+
 }
--- src/test/kotlin/PlayerTest.kt
+++ src/test/kotlin/PlayerTest.kt
@@ -1,3 +1,6 @@
+import InputValidator.inputWithPrediction
+import io.mockk.*
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.CsvSource
 import kotlin.test.assertEquals
@@ -13,5 +16,22 @@ internal class PlayerTest {
     @CsvSource("HUMAN; You are the predictor, what is your input?", "AI; AI is the predictor, what is your input?", delimiter = ';')
     internal fun `should print correct ask for input message given current predicting Player`(playerEnum: String, askForInputMessage: String) {
         assertEquals(askForInputMessage, Player.valueOf(playerEnum).getAskForInputMessage())
+    }
+
+    @Test
+    internal fun `should generate HUMAN input`() {
+        unmockkAll()
+        mockkObject(Terminal)
+        every { Terminal.getInput(any()) } returns "OO2"
+        spyk(Player.HUMAN, recordPrivateCalls = true)
+
+        assertEquals(
+            PlayerInput("OO2", true),
+            Player.HUMAN.generateInput(Player.HUMAN)
+        )
+        verify {
+            Terminal.printMessage(any())
+            Terminal.getInput(::inputWithPrediction)
+        }
     }
 }
