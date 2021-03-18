2438416de53d61989a2d614c551f4f05082067dd
Natalie Chin <natalie.chin@thoughtworks.com>
2021-03-18 17:28:01 +0800

    Add happy single digit path for positiveInteger validator
    Signed-off-by: Natalie Chin <natalie.chin@thoughtworks.com>

--- src/test/kotlin/InputValidatorTest.kt
+++ src/test/kotlin/InputValidatorTest.kt
@@ -1,7 +1,11 @@
 import InputValidator.inputWithPrediction
 import InputValidator.inputWithoutPrediction
+import InputValidator.positiveInteger
 import InputValidator.yesNo
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import kotlin.test.assertNull
 
 internal class InputValidatorTest {
     @Test
@@ -29,5 +33,11 @@ internal class InputValidatorTest {
             "correct input should be of the form CC, where the first two letters indicate [O]pen or [C]losed state for each hand",
             inputWithoutPrediction("CC2")
         )
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [1, 2, 3, 4, 5, 6, 7, 8, 9])
+    internal fun `should validate positiveInteger responses`(input: Int) {
+        assertNull(positiveInteger(input.toString()))
     }
 }
